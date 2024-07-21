54b7ad6862a39b20d548215591985ca03f68293d
islamdev2022 <islam.birouk.2004@gmail.com>
2024-07-21 02:04:45 +0100

    fix frequntly hourly Messages

--- daily_motivation_bot.py
+++ daily_motivation_bot.py
@@ -2,10 +2,9 @@ import logging
 from dotenv import load_dotenv
 import os
 import requests
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
-from telegram.ext import JobQueue
-import datetime
+
 # Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -21,21 +20,18 @@ API_KEY = os.getenv('APIKEY')
 CATEGORY, QUOTE, FREQUENCY, SET_FREQUENCY = range(4)
 
 # List of categories
-categories = [
-    'alone', 'amazing', 'anger', 'art', 'attitude', 'beauty',
-    'best', 'birthday', 'business', 'car', 'change', 'communication', 'computers', 
-    'cool', 'courage', 'dad', 'dating', 'death', 'design', 'dreams', 'education', 
-    'environmental', 'equality', 'experience', 'failure', 'faith', 'family', 'famous',
+categories = [ 'amazing',
+    'best', 'birthday', 'business', 'car', 'change', 'communication', 
+    'cool', 'courage', 'dad', 'dating', 'dreams', 
+    'equality', 'experience', 'failure', 'faith', 'family', 
     'fear', 'fitness', 'food', 'forgiveness', 'freedom', 'friendship', 'funny', 
-    'future', 'god', 'good', 'government', 'graduation', 'great', 'happiness', 
+    'future', 'god', 'good', 'graduation', 'great', 'happiness', 
     'health', 'history', 'home', 'hope', 'humor', 'imagination', 'inspirational', 
-    'intelligence', 'jealousy', 'knowledge', 'leadership', 'learning', 'legal', 
-    'life', 'love', 'marriage', 'medical', 'men', 'mom', 'money', 'morning', 
-    'movies', 'success'
+    'intelligence',  'knowledge', 'leadership', 'learning', 
+    'life', 'love', 'marriage', 'men', 'mom', 'money', 'success'
 ]
 
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user = update.message.from_user
     username = user.username if user.username else user.first_name
 
@@ -52,18 +48,16 @@ async def set_category(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
     user = update.message.from_user
     category = update.message.text
     username = user.username if user.username else user.first_name
-    logger.info("Category chosen by %s: %s", user.first_name, category)
 
     context.user_data['category'] = category
     
     await update.message.reply_text(
         f'Thank you {username}! You have chosen {category} category. '
         'Now please choose how often you want to receive motivational quotes:',
-        reply_markup=ReplyKeyboardMarkup([['1 hour', '2 hours', '4 hours', '8 hours','12 hours']], one_time_keyboard=True)
+        reply_markup=ReplyKeyboardMarkup([['1 Hour', '2 Hours', '4 Hours', '8 Hours', '12 Hours']], one_time_keyboard=True)
     )
 
     return FREQUENCY
-
 
 async def set_frequency(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user = update.message.from_user
@@ -71,67 +65,70 @@ async def set_frequency(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
     username = user.username if user.username else user.first_name
 
     frequency_mapping = {
-        '1 hour': 1,
-        '2 hours': 2,
-        '4 hours': 4,
-        '8 hours': 8,
-        '12 hours': 12
+        '1 Hour': 1,
+        '2 Hours': 2,
+        '4 Hours': 4,
+        '8 Hours': 8,
+        '12 Hours': 12
     }
 
-    frequency_hours = frequency_mapping.get(frequency_text)
+    frequency_Hours = frequency_mapping.get(frequency_text)
 
-    if not frequency_hours:
+    if not frequency_Hours:
         await update.message.reply_text(
             'Invalid frequency selection. Please choose from the provided options.',
-            reply_markup=ReplyKeyboardMarkup([['1 hour', '2 hours', '4 hours', '8 hours','12 hours']], one_time_keyboard=True)
+            reply_markup=ReplyKeyboardMarkup([['1 Hour', '2 Hours', '4 Hours', '8 Hours','12 Hours']], one_time_keyboard=True)
         )
         return FREQUENCY
 
-    context.user_data['frequency'] = frequency_hours
+    context.user_data['frequency'] = frequency_Hours
+    category = context.user_data['category']
 
     await update.message.reply_text(
-        f'Great choice, {username}! You will now receive motivational quotes every {frequency_hours} hours.'
+        f'Great choice, {username}! You will now receive motivational quotes every {frequency_Hours} Hours.'
         ' You can type /NewQuote anytime to receive a quote immediately.'
     )
 
     # Schedule the first job immediately
-    context.job_queue.run_repeating(send_daily_quote, interval=frequency_hours * 3600, first=0, context=update.message.chat_id)
+    context.job_queue.run_repeating(send_daily_quote, interval=frequency_Hours * 3600, first=0, data={'chat_id': update.message.chat_id, 'category': category})
 
     return ConversationHandler.END
-
 
 async def fetch_quote(category: str) -> str:
     api_url = f'https://api.api-ninjas.com/v1/quotes?category={category}'
     response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
-    if response.status_code == requests.codes.ok:
-        data = response.json()
-        if data:
-            return data[0]['quote']
-    return "Could not fetch quote at this time. Please try again later."
+
+    if response.status_code != requests.codes.ok:
+        logger.error(f"Error fetching quote: {response.status_code} - {response.text}")
+        return "Could not fetch quote at this time. Please try again later."
+
+    data = response.json()
+    if not data:
+        logger.error(f"No data received from API: {response.text}")
+        return "Could not fetch quote at this time. Please try again later."
+
+    return data[0]['quote']
 
 async def send_daily_quote(context: ContextTypes.DEFAULT_TYPE) -> None:
-    job = context.job
-    user_data = context.job.context
-    category = user_data.get('category', 'happiness')
+    job_data = context.job.data
+    category = job_data.get('category', 'happiness')
+    chat_id = job_data.get('chat_id')
     quote = await fetch_quote(category)
-    await context.bot.send_message(job.context, text=quote)
-    
-    
+    await context.bot.send_message(chat_id=chat_id, text=quote)
+
 async def daily_quote(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     category = context.user_data.get('category', 'happiness')
     quote = await fetch_quote(category)
     await update.message.reply_text(quote)
 
-
 def main() -> None:
-    # Replace 'YOUR_TOKEN_HERE' with your bot's token
     application = Application.builder().token(token).build()
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
             CATEGORY: [MessageHandler(filters.Regex(f'^({"|".join(categories)})$'), set_category)],
-            FREQUENCY: [MessageHandler(filters.Regex(f'^({"|".join(['1 hour', '2 hours', '4 hours', '8 hours','12 hours'])})$'), set_frequency)]
+            FREQUENCY: [MessageHandler(filters.Regex(f'^({"|".join(["1 Hours", "10 Hours", "15 Hours", "30 Hours", "60 Hours"])})$'), set_frequency)]
         },
         fallbacks=[CommandHandler('start', start)]
     )
